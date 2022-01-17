b2ca27b9e3e0aaffdb88f873ad908138c9607828
Leroy <just-leroy@hotmail.com>
2022-01-17 13:27:54 +0100

    Initial Commit

---
+++ swiftinteraction/swiftinteractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftinteractionApp.swift
+//  swiftinteraction
+//
+//  Created by Leroy on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftinteractionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
